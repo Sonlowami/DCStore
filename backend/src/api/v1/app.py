542c8bc462d57bb2180f83a3394e2eaffcbafad5
saad-out <outsaad03@gmail.com>
2023-09-14 19:24:45 +0100

    remove test views

--- backend/src/api/v1/app.py
+++ backend/src/api/v1/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 
 from os import getenv
 from dotenv import load_dotenv
 
 from api.v1.utils.database import mongo
 from api.v1.utils.config import Config
-from api.v1.models.user import User
 
 
 # Load environment variables
@@ -22,46 +21,6 @@ mongo.init_app(app)
 @app.route('/')
 def index():
     return 'Hello World!'
-
-# Insert todo item into MongoDB
-@app.route('/todo', methods=['GET'])
-def insert_todo():
-    todo = {
-        'title': 'Test todo',
-        'description': 'Test todo description',
-        'done': False,
-    }
-    mongo.db.todos.insert_one(todo) # type: ignore
-    return 'Todo inserted!'
-
-# Get all todo items from MongoDB
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    todos = mongo.db.todos.find() # type: ignore
-    return jsonify([todo['title'] for todo in todos])
-
-# Get all users from MongoDB
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.get_users()
-    return jsonify(users)
-
-# Create user
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    try:
-        user = User(
-            email=data.get('email'),
-            password=data.get('password'),
-            username=data.get('username'),
-            fullname=data.get('fullname'),
-            role=data.get('role'),
-        )
-    except Exception as e:
-        return jsonify( { "error": "Invalid user schema" } ), 400
-    User.save(user)
-    return jsonify(user), 201
 
 
 if __name__ == '__main__':

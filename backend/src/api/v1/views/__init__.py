724eab0cacb2bdc641691aa5b4fd4170800ad23a
saad-out <outsaad03@gmail.com>
2023-09-22 15:15:19 +0100

    add studies GET endpoints

--- backend/src/api/v1/views/__init__.py
+++ backend/src/api/v1/views/__init__.py
@@ -5,3 +5,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.auth import *
 from api.v1.views.files import *
 from api.v1.views.patients import *
+from api.v1.views.studies import *
